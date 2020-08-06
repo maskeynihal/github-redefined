b411d8b9a6fe2b212606142e5b88f2b667185c49
maskeynihal <maskeynihal@gmail.com>
2020-08-06 13:27:37 +0545

    Updates

---
+++ precache-manifest.5e0951feb5fc4008f49d5ec9cd56ce48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd97b90098a8a8d308e14eadbfd6abda",
+    "url": "/github-redefined/index.html"
+  },
+  {
+    "revision": "8ff27795b3860885c18f",
+    "url": "/github-redefined/static/css/main.73d4bb89.chunk.css"
+  },
+  {
+    "revision": "c66c45759818ca881597",
+    "url": "/github-redefined/static/js/2.7a5a8849.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-redefined/static/js/2.7a5a8849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ff27795b3860885c18f",
+    "url": "/github-redefined/static/js/main.3559b094.chunk.js"
+  },
+  {
+    "revision": "821c08f2fdab10cd95bd",
+    "url": "/github-redefined/static/js/runtime-main.2706f254.js"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/github-redefined/static/media/logo.1bb02710.svg"
+  }
+]);
